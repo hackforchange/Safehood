3dc5e4b75ebf965808ef6371579f5ab9f61c84f5
T S <YenTheFirst@gmail.com>
2011-06-18 15:50:38 -0700

    define 'receive' method for SMS

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
   has_many :messages
+  #assume a maximum radius of 1km
+  
+  def nearby_users
+    #TODO
+    #all users who are within subscription range
+    User.all
+  end
+
 end
