7c63af85407f8ca48b75fc760f867533e6d81cd2
T S <YenTheFirst@gmail.com>
2011-06-18 15:07:10 -0700

    added flocky for SMS

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ gem 'rails', '3.0.6'
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
+gem 'flocky', :git => 'git://github.com/codeforamerica/flocky.git'
 gem 'sqlite3'
 
 # Use unicorn as the web server
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Safehood::Application.initialize!
+
+$outbound_flocky = Flocky.new ENV['FLOCKY_TOKEN'],ENV['FLOCKY_APPNUM'],{:username=>ENV['FLOCKY_USERNAME'],:password=>ENV['FLOCKY_PASSWORD']}, :queue => false
