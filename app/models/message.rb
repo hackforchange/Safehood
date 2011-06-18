8ebb731ff3c781390c71cde0e5eee717c8ad0ece
T S <YenTheFirst@gmail.com>
2011-06-18 16:17:31 -0700

    added 'backlogged' method to messages, for a phone number

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,7 @@
 class Message < ActiveRecord::Base
   belongs_to :user
+  
+  def self.backlogged(phone_number)
+    where("user_id is null AND phone = ? AND created_at > ?",phone_number,Date.yesterday)
+  end
 end
