885edf56d34d4b249afa8caa1c9392f7f416e381
Karen Rustad <karen.rustad@gmail.com>
2011-06-18 20:37:40 -0700

    Added fake 'messages' to front page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   def index
+    @messages = Message.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @messages }
+    end
   end
   def about
   end
