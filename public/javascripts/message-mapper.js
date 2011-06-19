45ea53f86c1b982742802b2baa25343e6d088615
Alan Palazzolo <github@zzolo.org>
2011-06-18 17:55:11 -0700

    Updated map to center around HfC.

--- public/javascripts/message-mapper.js
+++ public/javascripts/message-mapper.js
@@ -10,8 +10,23 @@ if (typeof L != 'undefined' && typeof jQuery != 'undefined') {
         var cloudmadeUrl = 'http://{s}.tile.cloudmade.com/90480db8a5a4470d87c3c21800806e02/997/256/{z}/{x}/{y}.png';
         var cloudmadeAttrib = 'Map data &copy; 2011 OpenStreetMap contributors, Imagery &copy; 2011 CloudMade';
         var cloudmade = new L.TileLayer(cloudmadeUrl, {maxZoom: 18, attribution: cloudmadeAttrib});
-        var london = new L.LatLng(51.505, -0.09);
-        map.setView(london, 13).addLayer(cloudmade);
+        var london = new L.LatLng(37.77917, -122.390903);
+        map.setView(london, 14).addLayer(cloudmade);
+        
+        var markerLocation = new L.LatLng(37.77918, -122.390953);
+        var marker = new L.Marker(markerLocation);
+        map.addLayer(marker);
+        var circleLocation = new L.LatLng(37.77915, -122.395003),
+            circleOptions = {
+                color: 'red', 
+                fillColor: '#f03', 
+                fillOpacity: 0.5
+            };
+            
+        var circle = new L.Circle(circleLocation, 100, circleOptions);
+        map.addLayer(circle);
+        marker.bindPopup("<b>Hello world!</b><br />I am a popup.");
+        circle.bindPopup("I am a circle.");
       }
     });
   })(jQuery);
