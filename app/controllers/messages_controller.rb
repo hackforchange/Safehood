7e3968ae97ce0b49cc83ca2fcc6bd7ecb5857544
T S <YenTheFirst@gmail.com>
2011-06-19 02:26:05 -0700

    actually, page=all is pretty silly. allow a custom per_page, with a limit of 1k

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,7 +10,8 @@ class MessagesController < ApplicationController
     m=m.textsearch(params[:q]) if params[:q]
     m=m.where("created_at >= ?", params[:date_min]) if params[:date_min]
     m=m.where("created_at <= ?", params[:date_max]) if params[:date_max]
-    params[:page] == "all" ? m.all : m.paginate(:page=>params[:page],:order=>"created_at DESC")
+    params[:per_page] = [params[:per_page].to_i,1000].min if params[:per_page]
+    m.paginate(:page=>params[:page],:per_page=>params[:per_page],:order=>"created_at DESC")
   end
   
   def index
