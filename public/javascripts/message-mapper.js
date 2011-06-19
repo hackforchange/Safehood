b6dc079d1c2b958146fbaa994d9969c6fbe7f75b
Alan Palazzolo <github@zzolo.org>
2011-06-18 17:28:41 -0700

    Added jquery and basic leaflet map.

---
+++ public/javascripts/message-mapper.js
@@ -0,0 +1,16 @@
+// Mapping logic
+
+if (typeof L != 'undefined' && typeof jQuery != 'undefined') {
+  jQuery.noConflict();
+  
+  (function($) {
+    $(document).ready(function() {
+      var map = new L.Map('map-messages');
+      var cloudmadeUrl = 'http://{s}.tile.cloudmade.com/90480db8a5a4470d87c3c21800806e02/997/256/{z}/{x}/{y}.png';
+      var cloudmadeAttrib = 'Map data &copy; 2011 OpenStreetMap contributors, Imagery &copy; 2011 CloudMade';
+      var cloudmade = new L.TileLayer(cloudmadeUrl, {maxZoom: 18, attribution: cloudmadeAttrib});
+      var london = new L.LatLng(51.505, -0.09);
+      map.setView(london, 13).addLayer(cloudmade);
+    });
+  })(jQuery);
+}
