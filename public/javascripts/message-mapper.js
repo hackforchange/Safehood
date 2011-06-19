194a2372eb3fd17720599360b75b1ffe013380f1
Alan Palazzolo <github@zzolo.org>
2011-06-18 18:26:38 -0700

    Added test data to map.

--- public/javascripts/message-mapper.js
+++ public/javascripts/message-mapper.js
@@ -1,5 +1,38 @@
 // Mapping logic
 
+var testData = [
+  {
+    'id': '8374987349873',
+    'message': 'This is a test message',
+    'lat': 37.77918,
+    'lon': -122.390953,
+    'location': 'Townsend and 2nd, San Francisco, CA',
+    'hashed_phone': '#3493oijf34ij4non4rij43900',
+    'hashed_ip': '#ndj98fehreef9fjsdf09',
+    'timestamp': new Date()
+  },
+  {
+    'id': '3874598374598743',
+    'message': 'This is a test message 02',
+    'lat': 37.77918,
+    'lon': -122.390953,
+    'location': 'Townsend and 2nd, San Francisco, CA',
+    'hashed_phone': '#3493oijf34ij4non4rij43900',
+    'hashed_ip': '#ndj98fehreef9fjsdf09',
+    'timestamp': new Date()
+  },
+  {
+    'id': '2772727272772',
+    'message': 'This is a test message 03',
+    'lat': 37.77918,
+    'lon': -122.390953,
+    'location': 'Townsend and 2nd, San Francisco, CA',
+    'hashed_phone': '#3493oijf34ij4non4rij43900',
+    'hashed_ip': '#ndj98fehreef9fjsdf09',
+    'timestamp': new Date()
+  }
+];
+
 if (typeof L != 'undefined' && typeof jQuery != 'undefined') {
   jQuery.noConflict();
   
@@ -13,20 +46,17 @@ if (typeof L != 'undefined' && typeof jQuery != 'undefined') {
         var london = new L.LatLng(37.77917, -122.390903);
         map.setView(london, 14).addLayer(cloudmade);
         
-        var markerLocation = new L.LatLng(37.77918, -122.390953);
-        var marker = new L.Marker(markerLocation);
-        map.addLayer(marker);
-        var circleLocation = new L.LatLng(37.77915, -122.395003),
-            circleOptions = {
-                color: 'red', 
-                fillColor: '#f03', 
-                fillOpacity: 0.5
-            };
-            
-        var circle = new L.Circle(circleLocation, 100, circleOptions);
-        map.addLayer(circle);
-        marker.bindPopup("<b>Hello world!</b><br />I am a popup.");
-        circle.bindPopup("I am a circle.");
+        markers = [];
+        for (var i in testData) {
+          testData[i].lat = (Math.random() > .5) ? testData[i].lat + (Math.random() * .005) :
+            testData[i].lat - (Math.random() * .005);
+          testData[i].lon = (Math.random() > .5) ? testData[i].lon + (Math.random() * .005) :
+            testData[i].lon - (Math.random() * .005);
+          
+          markers[i] = new L.Marker(new L.LatLng(testData[i].lat, testData[i].lon));
+          map.addLayer(markers[i]);
+          markers[i].bindPopup(testData[i].message);
+        }
       }
     });
   })(jQuery);
