dc7a6df85ae189aa52168c520ddbe681f6ee49e2
Alan Palazzolo <github@zzolo.org>
2011-06-18 17:33:19 -0700

    Updated map JS to check for DOM element.

--- public/javascripts/message-mapper.js
+++ public/javascripts/message-mapper.js
@@ -5,12 +5,14 @@ if (typeof L != 'undefined' && typeof jQuery != 'undefined') {
   
   (function($) {
     $(document).ready(function() {
-      var map = new L.Map('map-messages');
-      var cloudmadeUrl = 'http://{s}.tile.cloudmade.com/90480db8a5a4470d87c3c21800806e02/997/256/{z}/{x}/{y}.png';
-      var cloudmadeAttrib = 'Map data &copy; 2011 OpenStreetMap contributors, Imagery &copy; 2011 CloudMade';
-      var cloudmade = new L.TileLayer(cloudmadeUrl, {maxZoom: 18, attribution: cloudmadeAttrib});
-      var london = new L.LatLng(51.505, -0.09);
-      map.setView(london, 13).addLayer(cloudmade);
+      if ($('#map-messages').length) {
+        var map = new L.Map('map-messages');
+        var cloudmadeUrl = 'http://{s}.tile.cloudmade.com/90480db8a5a4470d87c3c21800806e02/997/256/{z}/{x}/{y}.png';
+        var cloudmadeAttrib = 'Map data &copy; 2011 OpenStreetMap contributors, Imagery &copy; 2011 CloudMade';
+        var cloudmade = new L.TileLayer(cloudmadeUrl, {maxZoom: 18, attribution: cloudmadeAttrib});
+        var london = new L.LatLng(51.505, -0.09);
+        map.setView(london, 13).addLayer(cloudmade);
+      }
     });
   })(jQuery);
 }
